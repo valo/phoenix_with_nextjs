75a082d4d1c90c027cedfd2b30254891a6f2caba
Valentin Mihov <valentin.mihov@gmail.com>
2017-09-07 23:19:44 +0300

    Webpack config for setting the env vars in the client side
    This is needed so that the client side code have access to the ENV
    variables, like the URL of the API server.

---
+++ app/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  webpack: (cfg) => {
+    cfg.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.WEBSITE_URL': JSON.stringify(process.env.WEBSITE_URL),
+      })
+    );
+
+    return cfg;
+  },
+};
